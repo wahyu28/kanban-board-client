007eb4a5c42fba5958adfc5222624e40649cd9e3
Wahyu Pratama <wahyulucky28@gmail.com>
2022-10-03 14:27:51 +0700

    finish project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom"
+import Comments from "./components/Comments"
+import Task from "./components/Task"
+import Login from "./components/Login"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/task' element={<Task />} />
+        <Route path='/comments/:category/:id' element={<Comments />} />
+      </Routes>
     </div>
   );
 }
